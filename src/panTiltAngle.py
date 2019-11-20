26cfb3b159a804db92e5ac0533c60dc503e66272
Davis Landry <davislandry1@gmail.com>
2019-11-20 09:53:12 -0700

    working with custom message

--- src/panTiltAngle.py
+++ src/panTiltAngle.py
@@ -11,23 +11,13 @@ class Node():
         self.tiltTickVal = 2048
 
     def onPanAngle(self,data):
-        if data.data < 0 :
-            data.data = 0
-        if data.data > 360 :
-            data.data = 360
         self.panTickVal = int((data.data/360)*4095)
 
     def onTiltAngle(self,data):
-        if data.data < 0 :
-            data.data = 0
-        if data.data > 180 :
-            data.data = 180
         self.tiltTickVal = int((data.data/360)*4095 + 1024)
 
 
     def panTiltAngle(self):
-        #self.panTick = rospy.Publisher('/pan_goal_position', Int16, queue_size=1)
-        #self.tiltTick = rospy.Publisher('/tilt_goal_position', Int16, queue_size=1)
         self.panTiltTick = rospy.Publisher('/pan_tilt_control', panTiltControl, queue_size=1)
         
         rospy.Subscriber('pan_goal_angle', Float32, self.onPanAngle)
@@ -35,14 +25,9 @@ class Node():
         self.control = panTiltControl();
         rate = rospy.Rate(5) # 10hz
         while not rospy.is_shutdown():
-            # self.panTick.publish(self.panTickVal)
-            # self.tiltTick.publish(self.tiltTickVal)
             self.control.pan_position = self.panTickVal;
             self.control.tilt_position = self.tiltTickVal;
             self.panTiltTick.publish(self.control);
-            
-#            rospy.loginfo("Pan Tick Value: " + str(self.panTickVal))
- #           rospy.loginfo("Tilt Tick Value: " + str(self.tiltTickVal))
             rate.sleep()
         rospy.spin()
 

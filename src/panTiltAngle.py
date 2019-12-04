069ce09608de33ac673ffeb5ad99503f3fbffffa
Davis Landry <davislandry1@gmail.com>
2019-12-04 15:45:18 -0700

    working with speed and torque control

--- src/panTiltAngle.py
+++ src/panTiltAngle.py
@@ -4,33 +4,39 @@ import rospy
 from std_msgs.msg import Int16
 from std_msgs.msg import Float32
 from dynamixel.msg import panTiltControl
+from dynamixel.msg import panTiltInternalControl
 
 class Node():
     def __init__(self):
         self.panTickVal = 2048
         self.tiltTickVal = 2048
+        self.panTorque = True;
+        self.tiltTorque = True;
+        self.panMaxSpeed = 0;
+        self.tiltMaxSpeed = 0;
 
-    def onPanAngle(self,data):
-        self.panTickVal = int((data.data/360)*4095)
-
-    def onTiltAngle(self,data):
-        self.tiltTickVal = int((data.data/360)*4095 + 1024)
+    def onPanTiltControl(self,data):
+        self.panTickVal   = int((data.pan_position/360)*4095)
+        self.tiltTickVal  = int((data.tilt_position/360)*4095 + 1024)
+        self.panTorque    = data.pan_torque
+        self.tiltTorque   = data.tilt_torque
+        self.panMaxSpeed  = int((data.pan_max_speed/116.62)*1024)
+        self.tiltMaxSpeed = int((data.tilt_max_speed/116.62)*1024)
 
 
     def panTiltAngle(self):
-        self.panTiltTick = rospy.Publisher('/pan_tilt_control', panTiltControl, queue_size=1)
+        self.panTiltTick = rospy.Publisher('/pan_tilt_internal_control', panTiltInternalControl, queue_size=1)
         
-        rospy.Subscriber('pan_goal_angle', Float32, self.onPanAngle)
-        rospy.Subscriber('tilt_goal_angle', Float32, self.onTiltAngle)
-        self.control = panTiltControl();
-        self.control.pan_max_speed = 0;
-        self.control.pan_torque = True;
-        self.control.tilt_max_speed = 0;
-        self.control.tilt_torque = True;
-        rate = rospy.Rate(5) # 10hz
+        rospy.Subscriber('pan_tilt_control', panTiltControl, self.onPanTiltControl)
+        self.control = panTiltInternalControl();
+        rate = rospy.Rate(5) # 5hz
         while not rospy.is_shutdown():
             self.control.pan_position = self.panTickVal;
             self.control.tilt_position = self.tiltTickVal;
+            self.control.pan_max_speed = self.panMaxSpeed;
+            self.control.tilt_max_speed = self.tiltMaxSpeed;
+            self.control.pan_torque = self.panTorque;
+            self.control.tilt_torque = self.tiltTorque;
             self.panTiltTick.publish(self.control);
             rate.sleep()
         rospy.spin()
--- src/sweep.py
+++ src/sweep.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Int16
 from std_msgs.msg import Float32
+from dynamixel.msg import panTiltControl
 
 class Node():
     def __init__(self):
@@ -11,26 +12,31 @@ class Node():
 
    
     def panTiltAngle(self):
-        self.panTick = rospy.Publisher('pan_goal_angle', Float32, queue_size=1)
-        self.tiltTick = rospy.Publisher('tilt_goal_angle', Float32, queue_size=1)
+        self.panTilt = rospy.Publisher('pan_tilt_control', panTiltControl, queue_size=1)
         rate = rospy.Rate(5) # 10hz
         negative_pan = 1
         negative_tilt = 1
         while not rospy.is_shutdown():
-            if self.panTickVal == 360 or self.panTickVal == 0:
+            if self.panTickVal >= 360 or self.panTickVal <= 0:
                 negative_pan = -1*negative_pan
                 
             self.panTickVal = self.panTickVal + 20*negative_pan
            
                         
-            if self.panTickVal == 360 or self.panTickVal == 0:
-                if self.tiltTickVal == 180 or self.tiltTickVal == 0:
+            if self.panTickVal >= 360 or self.panTickVal <= 0:
+                if self.tiltTickVal >= 180 or self.tiltTickVal <= 0:
                     negative_tilt = -1*negative_tilt
 
                 self.tiltTickVal = self.tiltTickVal + 20*negative_tilt
-            
-            self.panTick.publish(self.panTickVal)
-            self.tiltTick.publish(self.tiltTickVal)
+
+            self.control = panTiltControl();
+            self.control.pan_position = self.panTickVal;
+            self.control.tilt_position = self.tiltTickVal;
+            self.control.pan_torque = True;
+            self.control.tilt_torque = True;
+            self.control.pan_max_speed = 0;
+            self.control.tilt_max_speed = 0;
+            self.panTilt.publish(self.control)
             rospy.loginfo("Pan Tick Value: " + str(self.panTickVal))
             rospy.loginfo("Tilt Tick Value: " + str(self.tiltTickVal))
             rate.sleep()
